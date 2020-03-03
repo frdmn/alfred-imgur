f50ef5118940be396f4d6ff26dd87f9a4ca1941a
Jonas Friedmann <j@frd.mn>
2020-03-03 23:37:08 +0100

    Copy link to uploaded file to clipboard

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const alfy = require('alfy');
 const dotenv = require('dotenv');
 const imgur = require('imgur');
+const clipboardy = require('clipboardy');
 
 // Load config from ~/.alfred.ldap
 dotenv.config({path: path.join(os.homedir(), '.alfred-imgur.conf')});
@@ -15,6 +16,7 @@ if (alfy.input === 'errorNoSelection') {
 } else {
 	imgur.setClientId(process.env.APIKEY);
 	imgur.uploadFile(alfy.input).then(function (json) {
+		clipboardy.writeSync(json.data.link);
 		console.log(json.data.link);
 	}).catch(function (err) {
 		console.error(err.message);
