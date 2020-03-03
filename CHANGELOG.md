6716b9bc7d9dbf27899a0a40ce6942a1d7171020
Jonas Friedmann <j@frd.mn>
2020-03-03 23:24:54 +0100

    Rewrite in Node

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+### 1.2.0
+[Full Changelog](https://github.com/frdmn/alfred-ldap/compare/1ca0ed7...1.2.0)
+
+* Complete rewrite in NodeJS and with [Alfy](https://github.com/sindresorhus/alfy)
+
+### 1.1.0
+[Full Changelog](https://github.com/frdmn/alfred-imgur/compare/2f3251f...1ca0ed7)
+
+* Initial release
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+const os = require('os');
+const path = require('path');
+const alfy = require('alfy');
+const dotenv = require('dotenv');
+const imgur = require('imgur');
+
+// Load config from ~/.alfred.ldap
+dotenv.config({path: path.join(os.homedir(), '.alfred-imgur.conf')});
+
+if (alfy.input === 'errorNoSelection') {
+	console.log('Error: no file selected');
+} else if (alfy.input === 'errorFolder') {
+	console.log('Error: folder detected - please select a file');
+} else {
+	imgur.setClientId(process.env.APIKEY);
+	imgur.uploadFile(alfy.input)
+    .then(function (json) {
+        console.log(json.data.link);
+	})
+	.catch(function (err) {
+        console.error(err.message);
+    });
+}
