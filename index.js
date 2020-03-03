ca3a6db3d525183f9f5fd3a9d6bf0e70dad4360f
Jonas Friedmann <j@frd.mn>
2020-03-03 23:39:01 +0100

    Add some code comments

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ const clipboardy = require('clipboardy');
 // Load config from ~/.alfred.ldap
 dotenv.config({path: path.join(os.homedir(), '.alfred-imgur.conf')});
 
+// Check for errors
 if (alfy.input === 'errorNoSelection') {
 	console.log('Error: no file selected');
 } else if (alfy.input === 'errorFolder') {
 	console.log('Error: folder detected - please select a file');
 } else {
+	// Set client id
 	imgur.setClientId(process.env.APIKEY);
+	// Upload file to Imgur
 	imgur.uploadFile(alfy.input).then(function (json) {
+		// Copy upload URL to clipboard and write to stdout
 		clipboardy.writeSync(json.data.link);
 		console.log(json.data.link);
 	}).catch(function (err) {
-		console.error(err.message);
+		console.error('Error: ' + err.message);
 	});
 }
